3278a85f128d12bbce49a616f9cfe6de8e56d851
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-02-26 16:52:01 +0700

    edit delete suggest question of current_user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   load_and_authorize_resource
+  before_action :load_subjects, only: [:new, :edit]
 
   def index
     @questions = current_user.questions
@@ -10,7 +11,6 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @subjects = Subject.all
   end
 
   def create
@@ -23,7 +23,33 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @question.update_attributes question_params
+      flash[:success] = t "question.update_success"
+      redirect_to @question
+    else
+      flash[:warning] = t "question.update_failed"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      flash[:success] = t "question.delete_success"
+    else
+      flash[:danger] = t "question.delete_failed"
+    end
+    redirect_to questions_path
+  end
+
   private
+  def load_subjects
+    @subjects = Subject.all
+  end
+
   def question_params
     params.require(:question).permit :id, :subject_id, :content, :status,
       :user_id, question_answers_attributes: [:id, :content, :correct,
