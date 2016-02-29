482fc077a8515343f66bdfffc59e5fff757ddb5f
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-02-29 14:47:03 +0700

    create lesson of current_user

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,8 +4,20 @@ class LessonsController < ApplicationController
   def index
     @lessons = @lessons.page(params[:page]).per Settings.per_page
     @subjects = Subject.all
+    @lesson = current_user.lessons.new
   end
 
   def create
+    if @lesson.save
+      flash[:success] = t "lesson.create_success"
+    else
+      flash[:danger] = t "lesson.create_failed"
+    end
+    redirect_to lessons_path
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit :id, :user_id, :subject_id
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,4 +6,23 @@ class Lesson < ActiveRecord::Base
 
   has_many :results
   has_many :questions, through: :results
+
+  before_create :create_questions
+
+  accepts_nested_attributes_for :results, allow_destroy: true
+
+  validate :questions_less_than_require, on: :create
+
+  private
+  def questions_less_than_require
+    errors.add :create, I18n.t("lesson.create_failed") if self.subject.
+      questions.approved.size < Settings.number_questions_of_lesson
+  end
+
+  def create_questions
+    self.subject.questions.approved.order("RANDOM()").limit(Settings.
+      number_questions_of_lesson).each do |question|
+        self.results.build question_id: question.id
+      end
+  end
 end
